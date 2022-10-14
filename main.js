31d57b61030257d6432406afa9057a26aa7f4458
douglas-cavalcante <henriquedouglas2803@gmail.com>
2022-10-14 19:03:25 -0300

    adiciona nodemailer

--- main.js
+++ main.js
@@ -2,12 +2,47 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const { v4: uuidv4 } = require('uuid');
+const cron = require('node-cron')
+const nodemailer = require('nodemailer')
 
 let pizzas = []
 let solicitations = []
 
 app.use(express.json())
+app.use(cors())
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b2ab7e798f588d",
+    pass: "d0eae85bc4a4c8"
+  }
+});
+
+cron.schedule('*/1 * * * *', () => {
+
+  solicitations.forEach(solicitation => {
+    if (solicitation.order === "EM PRODUÇÃO") {
+      transporter.sendMail({
+        from: 'devinhouseteste@gmail.com',
+        to: 'joao@gmail.com',
+        subject: 'Sua pizza está chegando',
+        html: `<p>Olá ${solicitation.name_client}, sua pizza está há producao.</p>`
+      }, (err, info) => {
+        if (err)
+          console.log(err);
+        else
+          console.log(info);
+      })
+    }
+  });
+
+
+})
+
 
 app.get('/pizzas', (request, response) => {
   const nameQuery = request.query.name || ""
@@ -20,21 +55,20 @@ app.post('/pizzas', (request, response) => {
 
   const pizzaExists = pizzas.find(pizza => pizza.name === name)
 
-  if(pizzaExists) {
-    return response.status(401).json({error: 'Pizza já encontra-se cadastrada'})
+  if (pizzaExists) {
+    return response.status(401).json({ error: 'Pizza já encontra-se cadastrada' })
   }
 
-  const pizza = { 
+  const pizza = {
     id: uuidv4(),
-    name, 
+    name,
     url,
-    description, 
+    description,
     price,
-    ingredients 
+    ingredients
   }
 
   pizzas.push(pizza)
-  
 
   response.status(201).json(pizza)
 })
@@ -44,10 +78,18 @@ app.get('/solicitations', (request, response) => {
 })
 
 
+app.get('/solicitations/:id', (request, response) => {
+  const { id } = request.params
+
+  const solicitation = solicitations.find(solicitation => solicitation.id === id)
+
+  return response.json(solicitation)
+})
+
 app.post('/solicitations', (request, response) => {
 
   const {
-    name_client, 
+    name_client,
     document_client,
     contact_client,
     address_client,
@@ -58,7 +100,7 @@ app.post('/solicitations', (request, response) => {
 
   const soliciation = {
     id: uuidv4(),
-    name_client, 
+    name_client,
     document_client,
     contact_client,
     address_client,
